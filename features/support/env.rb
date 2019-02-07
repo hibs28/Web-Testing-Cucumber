f62b0f7054256dde949ae26752c6f32e8f7c100f
Tech-A06 <hibah28@live.co.uk>
2019-02-07 14:19:37 +0000

    Created a feature using gherkin syntax, env file has a configure for rspec, capybara/cucumber and created a first test to load up the bbc page

---
+++ features/support/env.rb
@@ -0,0 +1,17 @@
+require 'capybara/cucumber'
+require 'rspec'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+  config.color = true
+end
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+Capybara.configure do |config|
+  config.ignore_hidden_elements = false
+  config.default_max_wait_time = 10
+  config.default_driver = :chrome
+end 
