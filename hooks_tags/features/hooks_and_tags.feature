22b8fa72d8c145fc2241915540e264591b2838f4
Tech-A06 <hibah28@live.co.uk>
2019-02-11 10:35:04 +0000

    created a new folder for the project hooks and tags

---
+++ hooks_tags/features/hooks_and_tags.feature
@@ -0,0 +1,7 @@
+Feature: hooks and tags
+
+  As a user I wish to see how cucumber uses tags and hooks
+
+  Scenario: Check for name
+    Given I have a before hook in place
+    Then I can validate the information in the before hook
---
+++ hooks_tags/features/step_definitions/hooks_and_tags_step_defs.rb
@@ -0,0 +1,7 @@
+Given("I have a before hook in place") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I can validate the information in the before hook") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
