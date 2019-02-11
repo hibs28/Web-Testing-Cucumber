e9f217a235cf34bcbacfc5f710e099cfda9eaf35
Tech-A06 <hibah28@live.co.uk>
2019-02-11 10:47:22 +0000

    created an array with names and run a test to validate the first name is Dave

--- hooks_tags/features/step_definitions/hooks_and_tags_step_defs.rb
+++ hooks_tags/features/step_definitions/hooks_and_tags_step_defs.rb
@@ -1,7 +1,7 @@
 Given("I have a before hook in place") do
-  pending # Write code here that turns the phrase above into concrete actions
+  p @users
 end
 
 Then("I can validate the information in the before hook") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(@users[0]).to eql ('Dave')
 end
---
+++ hooks_tags/features/support/hooks.rb
@@ -0,0 +1,5 @@
+Before do 
+  # p 'We are setting the users objects -------'
+  @users = ['Dave', 'Gill', 'Jon']
+  # p '----------------------------'
+end
